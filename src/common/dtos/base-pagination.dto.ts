0e3560f6740b20592ee602627a437b634c9d6f57
Victor - Trabalho <victor.samuelsantoss@gmail.com>
2025-05-28 14:01:18 -0300

    feat: add base paginatin dto

---
+++ src/common/dtos/base-pagination.dto.ts
@@ -0,0 +1,55 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsIn, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+export class BasePaginationRequestDto {
+  @ApiPropertyOptional({
+    description: 'Número da página para paginação',
+    example: 1,
+    default: 1,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  page?: number;
+
+  @ApiPropertyOptional({
+    description: 'Limite de registros por página',
+    example: 10,
+    default: 10,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  limit?: number;
+
+  @ApiPropertyOptional({
+    description: 'Parâmetro de Ordenação',
+    example: 'name',
+  })
+  @IsOptional()
+  @IsString()
+  orderBy?: string;
+
+  @ApiPropertyOptional({
+    description: 'Parâmetro que define a direção da ordenação',
+    example: 'ASC',
+    enum: ['ASC', 'DESC'],
+  })
+  @IsOptional()
+  @IsString()
+  @IsIn(['ASC', 'DESC'], {
+    message: 'A direção da ordenação deve ser ASC ou DESC',
+  })
+  sortBy?: 'ASC' | 'DESC';
+}
+
+export interface BasePaginationResponseDto<T> {
+  data: T[];
+  limit: number;
+  page: number;
+  total: number;
+  totalPages: number;
+}
