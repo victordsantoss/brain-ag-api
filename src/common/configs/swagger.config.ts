0afb95d9be6507a270dabefd169204cacd779a46
Victor - Trabalho <victor.samuelsantoss@gmail.com>
2025-05-28 15:28:52 -0300

    feat(config): add cors and swagger configs

---
+++ src/common/configs/swagger.config.ts
@@ -0,0 +1,16 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Brain Agriculture  API')
+    .setDescription(
+      'Esta API fornece recursos para gerenciar e acessar informações relacionadas ao sistema, incluindo operações CRUD, autenticação e integração com serviços externos.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+}
